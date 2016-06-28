a7d576392273a2763d8e5513eb5294cdc0687195
Long Hoang <long@mindworker.de>
2016-06-28 17:05:31 +0800

    Wrong name in package.json and multi file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import http from 'http';
+import * as util from './util.js';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end(util.getHello() + ' World\n');
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running...');
